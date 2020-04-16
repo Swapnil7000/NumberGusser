04e99dffa78ca33e76a32ea26d30a7f4ab817895
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-04-16 13:22:14 +0530

    Now,you can play number guesser

--- app/src/main/java/com/swapnil/numberguesser/MainActivity.java
+++ app/src/main/java/com/swapnil/numberguesser/MainActivity.java
@@ -14,7 +14,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     Button btn_guess, btn_plus, btn_minus;
     private int rnd_num = 0, txt_num = 0, computer_num, target, cmp1, cmp2;
-    TextView display, num, message,computer_guess,tar_num;
+    TextView display, num, message, computer_guess, tar_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +22,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_guess = (Button) findViewById(R.id.btn_guess);
         btn_minus = (Button) findViewById(R.id.btn_minus);
-        btn_plus = (Button) findViewById(R.id.btn_guess);
+        btn_plus = (Button) findViewById(R.id.btn_plus);
         display = findViewById(R.id.rnd_num);
-        tar_num=findViewById(R.id.txt_value);
-        computer_guess=findViewById(R.id.txt_compguess);
+        tar_num = findViewById(R.id.txt_value);
+        computer_guess = findViewById(R.id.txt_compguess);
 
         num = findViewById(R.id.txt_num);
         final int numb = new Random().nextInt(10);
@@ -45,23 +45,19 @@ public class MainActivity extends AppCompatActivity {
         final int computer_num = new Random().nextInt(10);
         cmp1 = Math.abs(txt_num - target);
         cmp2 = Math.abs(computer_num - target);
-        tar_num.setText(target+"");
-        computer_guess.setText(computer_num+"");
+        tar_num.setText(target + "");
+        computer_guess.setText(computer_num + "");
         if ((cmp1) < (cmp2) && (cmp1 != 0)) {
             Toast.makeText(MainActivity.this, "Player won!! as he is close by " + Math.abs(cmp1) + " number", Toast.LENGTH_SHORT).show();
-        }
-        else if (computer_num == target) {
+        } else if ((computer_num == target) && (cmp1 != cmp2)) {
             Toast.makeText(MainActivity.this, "Computer won!!", Toast.LENGTH_SHORT).show();
-        }
-        else if (txt_num == target) {
+        } else if ((txt_num == target) && (cmp1 != cmp2)) {
             Toast.makeText(MainActivity.this, "Absolutely geneous!!", Toast.LENGTH_SHORT).show();
-        }
-        else if ((cmp1) == (cmp2)) {
+        } else if ((cmp2) < (cmp1) && (cmp2 != 0)) {
+            Toast.makeText(MainActivity.this, "Computer won!! as it is close by " + Math.abs(cmp2) + " number", Toast.LENGTH_SHORT).show();
+        } else {
             Toast.makeText(MainActivity.this, "Match tied", Toast.LENGTH_SHORT).show();
 
-        }
-        else if ((cmp2) < (cmp1) && (cmp2 != 0)) {
-            Toast.makeText(MainActivity.this, "Computer won!! as it is close by " + Math.abs(cmp2) + " number", Toast.LENGTH_SHORT).show();
         }
     }
 
